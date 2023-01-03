77d86148eb255d5b5a5a169cb83a88737a328858
Umut Elicabuk <amiculus.umut@hotmail.de>
2023-01-03 11:18:16 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <n-config-provider :theme-overrides="theme">
+    <AppView />
+    <n-global-style />
+  </n-config-provider>
+</template>
+
+<script setup lang="ts">
+import type { GlobalThemeOverrides } from 'naive-ui'
+import { NConfigProvider, NGlobalStyle } from 'naive-ui'
+import { defineComponent } from 'vue'
+import AppView from '@/views/AppView.vue'
+
+const darkTheme: GlobalThemeOverrides = {
+  common: {
+    primaryColor: "#ec407a",
+  },
+  Button: {
+    textColor: "#ec407a",
+  },
+  //...
+}
+
+const lightTheme: GlobalThemeOverrides = {
+  common: {
+    primaryColor: "#000",
+  },
+  Button: {
+    textColor: "#000",
+  },
+  //...
+}
+
+const theme = lightTheme;
+
+defineComponent({
+  components: {
+    NConfigProvider,
+    AppView,
+  },
+})
+</script>
---
+++ src/views/AppView.vue
@@ -0,0 +1,82 @@
+<template>
+    <div id="content">
+        <n-page-header class="nav" title="Piglify" subtitle="Managing the life of 3 little piglets since 2023.">
+            <template #extra>
+                <n-space justify="space-between">
+                    <n-switch>
+                        <template #icon>
+                            <n-icon :component="MoonRegular"></n-icon>
+                        </template>
+                    </n-switch>
+                </n-space>
+            </template>
+            <template #avatar>
+                <n-avatar src="https://cdnimg103.lizhi.fm/user/2017/02/04/2583325032200238082_160x160.jpg" />
+            </template>
+        </n-page-header>
+        <n-layout has-sider>
+            <n-layout-sider bordered>
+                <n-menu :options="siderOptions" />
+            </n-layout-sider>
+            <n-layout-content>Test</n-layout-content>
+        </n-layout>
+        <n-page-header class="footer">
+            Made with love in Waldstadt, ver. {{ version }}
+        </n-page-header>
+    </div>
+</template>
+
+<script setup lang="ts">
+import { defineComponent } from 'vue'
+import { NPageHeader, NSpace, NButton, NSwitch, NIcon, NLayout, NLayoutContent, NLayoutSider, NMenu, NAvatar, NLayoutFooter, type MenuOption } from 'naive-ui'
+import { MoonRegular } from '@vicons/fa'
+import { version } from '/package.json'
+
+defineComponent({
+    components: {
+        NPageHeader,
+        NIcon,
+        MoonRegular,
+        NButton,
+        NLayout,
+        NLayoutContent,
+        NLayoutSider,
+        NLayoutFooter,
+        NMenu,
+        NAvatar,
+    }
+})
+
+const siderOptions: MenuOption[] = [
+    {
+        label: "Chores",
+        key: "chores",
+    },
+    {
+        label: "Groceries",
+        key: "groceries",
+    },
+]
+</script>
+
+<style scoped>
+.nav {
+    padding: 10px 15px;
+}
+
+.footer {
+    height: 50px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    border: 1px solid lightgrey;
+}
+
+#content {
+    display: grid;
+    grid-template-rows: auto 1fr auto;
+    min-height: 100vh;
+    margin: 0;
+    font-size: 1.5em;
+}
+</style>
