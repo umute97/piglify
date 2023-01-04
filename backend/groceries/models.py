b1d38569c9545335930c534d76438a0a14a0e1eb
Umut Elicabuk <amiculus.umut@hotmail.de>
2023-01-04 15:41:26 +0100

    Make default addition date timezone-aware

--- backend/groceries/models.py
+++ backend/groceries/models.py
@@ -1,9 +1,13 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 class Grocery(models.Model):
     name = models.CharField(max_length=50, null=False)
     qty = models.IntegerField(default=1, blank=False, null=False)
     location = models.CharField(max_length=50, null=True)
-    date_added = models.DateTimeField(auto_now_add=True)
+    date_added = models.DateTimeField(default=timezone.now)
     bought = models.BooleanField(default=False, blank=False, null=False)
+
+    def __str__(self):
+        return f"{self.name}: x{self.qty}, bought: {self.bought}"
