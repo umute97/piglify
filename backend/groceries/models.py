a3abd260077e175e1f9bd84c01270e0a27b4e248
Umut Elicabuk <amiculus.umut@hotmail.de>
2023-01-05 14:19:49 +0100

    Add contact field to groceries list

--- backend/groceries/models.py
+++ backend/groceries/models.py
@@ -1,6 +1,12 @@
 from django.db import models
 from django.utils import timezone
 
+USERS = [
+    ("umut", "Umut"),
+    ("jonny", "Jonny"),
+    ("simon", "Simon"),
+]
+
 # Create your models here.
 class Grocery(models.Model):
     name = models.CharField(max_length=50, null=False)
@@ -8,6 +14,7 @@ class Grocery(models.Model):
     location = models.CharField(max_length=50, null=True)
     date_added = models.DateTimeField(default=timezone.now)
     bought = models.BooleanField(default=False, blank=False, null=False)
+    contact = models.CharField(null=True, max_length=10, blank=False, choices=USERS)
 
     def __str__(self):
         return f"{self.name}: x{self.qty}, bought: {self.bought}"
--- backend/server/settings.py
+++ backend/server/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'corsheaders',
     'rest_framework',
     'django_filters',
     'groceries.apps.GroceriesConfig',
@@ -43,6 +44,8 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -50,6 +53,11 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",
+    "http://127.0.0.1:5173"
 ]
 
 REST_FRAMEWORK = {
