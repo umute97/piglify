7be2a90f3363c0e00e67d0846cf7ab6c595aacea
Umut Elicabuk <umut.elicabuk@cern.ch>
2023-01-16 14:32:21 +0100

    Add user field

--- backend/groceries/models.py
+++ backend/groceries/models.py
@@ -7,14 +7,18 @@ USERS = [
     ("simon", "Simon"),
 ]
 
-# Create your models here.
 class Grocery(models.Model):
     name = models.CharField(max_length=50, null=False)
     qty = models.IntegerField(default=1, blank=False, null=False)
     location = models.CharField(max_length=50, null=True)
     date_added = models.DateTimeField(default=timezone.now)
     bought = models.BooleanField(default=False, blank=False, null=False)
-    contact = models.CharField(null=True, max_length=10, blank=False, choices=USERS)
+    contact = models.ForeignKey("User", on_delete=models.SET_NULL, null=True, blank=False)
 
     def __str__(self):
         return f"{self.name}: x{self.qty}, bought: {self.bought}"
+
+class User(models.Model):
+    first = models.CharField(max_length=20, null=False)
+    last = models.CharField(max_length=20, null=True)
+    profile_pic = models.ImageField(upload_to="profiles/")
