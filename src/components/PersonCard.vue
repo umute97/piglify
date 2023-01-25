ef84ce39cd0aa096a112bb7cedca970248ec53eb
Umut Elicabuk <amiculus.umut@hotmail.de>
2023-01-25 22:48:28 +0100

    Person card skeleton

---
+++ src/components/PersonCard.vue
@@ -0,0 +1,22 @@
+<template>
+    <n-card>
+        <template #cover>
+            <img :src="profilePic" alt="A cool person." />
+        </template>
+    </n-card>
+</template>
+
+<script setup lang="ts">
+import { defineComponent } from 'vue';
+import { NCard } from 'naive-ui';
+
+defineComponent({
+    components: { NCard, }
+})
+
+defineProps(['profilePic'])
+</script>
+
+<style scoped>
+
+</style>
