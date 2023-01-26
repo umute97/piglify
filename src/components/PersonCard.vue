5c6da3fe7ac8275371b0de328048c758853120f7
Umut Elicabuk <umut.elicabuk@cern.ch>
2023-01-26 14:25:22 +0100

    Add chores done toggle button

--- src/components/PersonCard.vue
+++ src/components/PersonCard.vue
@@ -1,20 +1,46 @@
 <template>
-    <n-card>
+    <n-card :title="person.full_name">
         <template #cover>
-            <img :src="profilePic" alt="A cool person." />
+            <img :src="person.profile_pic" alt="A cool person." />
+        </template>
+        <template #action>
+            <n-button type="error" @click="toggleDone">
+                <template #icon>
+                    <n-icon v-if="person.done" :component="ThumbsUpRegular" />
+                    <n-icon v-else :component="ThumbsDownRegular" />
+                </template>
+                Done
+            </n-button>
         </template>
     </n-card>
 </template>
 
 <script setup lang="ts">
 import { defineComponent } from 'vue';
-import { NCard } from 'naive-ui';
+import { NCard, NButton, NIcon } from 'naive-ui';
+import { ThumbsUpRegular, ThumbsDownRegular } from '@vicons/fa';
+
+export interface Person {
+    id: number,
+    full_name: string,
+    first: string,
+    last: string,
+    profile_pic?: string,
+    done: boolean
+}
 
 defineComponent({
-    components: { NCard, }
+    components: { NCard, NButton, NIcon, ThumbsDownRegular, ThumbsUpRegular }
 })
 
-defineProps(['profilePic'])
+const emit = defineEmits(['toggleChoresDone'])
+const props = defineProps<{
+    person: Person
+}>()
+
+function toggleDone() {
+    emit('toggleChoresDone', props.person.done)
+}
 </script>
 
 <style scoped>
--- src/views/ChoresView.vue
+++ src/views/ChoresView.vue
@@ -1,13 +1,45 @@
 <template>
-    <div>
-
+    <div id="chores-container">
+        <person-card v-for="person in people" :person="person" @toggle-chores-done="toggleChoresDone"/>
     </div>
 </template>
 
 <script setup lang="ts">
+import PersonCard from '@/components/PersonCard.vue'
+import type { Person } from '@/components/PersonCard.vue'
+import { defineComponent, onMounted, reactive, ref, type Ref } from 'vue';
+import axios from 'axios';
+import { useUrlStore } from '@/stores/urls';
 
+defineComponent({
+    components: { PersonCard }
+})
+
+const urlStore = useUrlStore()
+
+let people: Ref<Person[]> = ref([])
+
+onMounted(async () => {
+    await axios.get(`${urlStore.backendIP}/users/`).then((response) => {
+        people.value = response.data.results
+        people.value.forEach((person) => {
+            person.done = true
+        })
+    })
+})
+
+function toggleChoresDone(done: boolean) {
+    //TODO: toggle server state
+    //TODO: get server state
+    //TODO: set local state
+}
 </script>
 
 <style scoped>
+#chores-container {
+    display: flex;
+    gap: 1rem;
+    padding: 1rem;
+}
 
 </style>
