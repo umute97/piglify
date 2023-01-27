6ada00c180629603e0f1c19c12d92f9d451cbb51
Umut Elicabuk <umut.elicabuk@cern.ch>
2023-01-27 15:11:15 +0100

    Add all done message

--- src/views/ChoresView.vue
+++ src/views/ChoresView.vue
@@ -2,12 +2,17 @@
     <div id="chores-container">
         <person-card v-for="person in people" :person="person" @toggle-chores-done="toggleChoresDone" />
     </div>
+    <div class="all-chores-done" v-if="allChoresDone">
+        All done! <n-icon :component="Check"/>
+    </div>
 </template>
 
 <script setup lang="ts">
 import PersonCard from '@/components/PersonCard.vue'
 import type { Person } from '@/components/PersonCard.vue'
-import { defineComponent, onMounted, reactive, ref, type Ref } from 'vue';
+import { NIcon } from 'naive-ui';
+import { Check } from '@vicons/fa';
+import { computed, defineComponent, onMounted, reactive, ref, type Ref } from 'vue';
 import axios from 'axios';
 import { useUrlStore } from '@/stores/urls';
 
@@ -18,7 +23,7 @@ export interface Chore {
     date_added: string
 }
 defineComponent({
-    components: { PersonCard }
+    components: { PersonCard, NIcon, Check }
 })
 
 const urlStore = useUrlStore()
@@ -53,6 +58,11 @@ async function toggleChoresDone(person: Person) {
         people.value[objIndex].done_date = response.data.done_date
     })
 }
+
+const allChoresDone = computed(() => {
+    console.log("changed")
+    return people.value.every(element => element.done)
+})
 </script>
 
 <style scoped>
@@ -60,5 +70,13 @@ async function toggleChoresDone(person: Person) {
     display: flex;
     gap: 1rem;
     padding: 1rem;
+}
+
+.all-chores-done {
+    display: flex;
+    font-size: 4em;
+    justify-content: center;
+    align-items: center;
+    gap: 2rem;
 }
 </style>
