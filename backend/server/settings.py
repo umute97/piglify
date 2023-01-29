70782d09d856d12f09b6443294cc8859a5dbce03
Umut Elicabuk <amiculus.umut@hotmail.de>
2023-01-29 20:32:42 +0100

    Start dockerizing django app

--- backend/server/settings.py
+++ backend/server/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
-import os.path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -19,18 +19,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
+SECRET_KEY = os.environ.get("SECRET_KEY")
+SECURE_SSL_REDIRECT = False
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-%=l5d7#cw!r$ndp3+p^7dyuuw$@k#uc**g$)6tcd0s=hdjigci'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
+DEBUG = int(os.environ.get("DEBUG", default=0))
 
 # Application definition
-
 INSTALLED_APPS = [
     'corsheaders',
     'rest_framework',
