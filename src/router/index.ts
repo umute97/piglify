c90b9acd704410e6e256caffa4e8a08330f4e43b
Umut Elicabuk <amiculus.umut@hotmail.de>
2023-02-05 11:25:37 +0100

    Make chores view default route

--- src/router/index.ts
+++ src/router/index.ts
@@ -6,6 +6,11 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
+      path: "/",
+      name: "default",
+      component: ChoresViewVue,
+    },
+    {
       path: "/groceries",
       name: "groceries",
       component: GroceriesViewVue,
